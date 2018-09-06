1f275ae515ac1728af7a4415511b8b605fa7daed
hurry143 <hhur200@gmail.com>
2018-09-05 21:42:34 -0700

    Renames source files.

---
+++ YATT_ItemCount.namespace.lua
@@ -0,0 +1,16 @@
+YATT_ItemCount = {
+  -- Basic properties
+  NAME = 'YATT_ItemCount';
+  SHORTNAME = 'YATT Item Count';
+  ABBR = 'TTIC';
+  VERSION = '1.2.0';
+  AUTHOR = 'hurry143';
+
+  -- Dependencies
+  LIBTT = LibStub('LibYATT');
+  LIBADDONMENU = LibStub('LibAddonMenu-2.0');
+
+  -- Constants
+  BANK_INDEX = 'bank';
+  CRAFTBAG_INDEX = 'craftbag'
+};
